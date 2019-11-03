79d38a25957af2f2a1ce2279a469e6d92b98e87e
Scapellato <eva.scapellato@student.hepl.be>
2019-11-03 17:56:48 +0100

    hello world added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Hello World");
+			Console.WriteLine("labo1 test github: appyer sur une touche pour fermer:");
+			Console.ReadKey();
 		}
 	}
 }
